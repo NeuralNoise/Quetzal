d4ca62c67c40accebae51d1bfa9f695227aaccc6
BlameDylan <dylan.seidt@gmail.com>
2016-01-12 16:47:24 -0600

    More Backend Stuff

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -4,22 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Memcached;
+use DB;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class BackendController extends BaseController
 {
     public function postGenerate(Request $request) {
-        try {
-            $this->validate($request, [
-                'title' => 'required|unique:posts|max:255',
-                'body' => 'required',
-            ]);
-        } catch(HttpResponseException $e) {
-            die('Error');
-        }
+        $this->validate($request, [
+            'fqdn' => 'required|string|max:16',
+            'ip' => 'required',
+        ]);
+
+        DB::table('records')->insert(['token' => str_random(32), 'fqdn' => $request->input('fqdn'), 'ip' => $request->input('ip')]);
     }
 
-    public function postDestroy() {
+    public function postDestroy(Request $request) {
         $this->validate($request, [
             'title' => 'required|unique:posts|max:255',
             'body' => 'required',
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,3 +9,4 @@
 $app->get('/', ['as' => 'index', 'uses' => 'FrontendController@getIndex']);
 
 $app->post('/generate', ['as' => 'generate', 'uses' => 'BackendController@postGenerate']);
+$app->post('/destroy', ['as' => 'destroy', 'uses' => 'BackendController@postDestroy']);
