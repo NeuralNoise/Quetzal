bba8886b5d2caff1449b840a7b55006efda35544
BlameDylan <dylan.seidt@gmail.com>
2016-01-17 12:53:23 -0600

    Move Cloudflare into a Service

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Memcached;
 use DB;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
-use GuzzleHttp;
+use App\Services\FlareService;
 
 class BackendController extends BaseController
 {
@@ -17,28 +17,8 @@ class BackendController extends BaseController
             'ip' => 'required|ip|unique:records',
         ]);
 
-        $client = new GuzzleHttp\Client(['base_uri' => 'https://api.cloudflare.com']);
-
-        $key = json_decode($client->request('GET', '/client/v4/zones?name='.env('CLOUDFLARE_DOMAIN').'&status=active&page=1&per_page=1', [
-            'headers' => [
-                'X-Auth-Email' => env('CLOUDFLARE_EMAIL'),
-                'X-Auth-Key' => env('CLOUDFLARE_KEY')
-            ]
-        ])->getBody()->getContents())->result['0']->id;
-
-        $record = json_decode($client->request('POST', '/client/v4/zones/'.$key.'/dns_records', [
-            'headers' => [
-                'X-Auth-Email' => env('CLOUDFLARE_EMAIL'),
-                'X-Auth-Key' => env('CLOUDFLARE_KEY')
-            ],
-            'json' => [
-                'type' => 'A',
-                'name' => $request->input('fqdn').config('cloudflare.domain'),
-                'content' => $request->input('ip')
-            ]
-        ])->getBody()->getContents())->result->id;
-
-        DB::table('records')->insert(['token' => $record, 'fqdn' => $request->input('fqdn'), 'ip' => $request->input('ip')]);
+        $flare = new FlareService;
+        $record = $flare->create($request->input('fqdn'), $request->input('ip'));
 
         return redirect()->route('index')->with('key', $record);
     }
@@ -48,23 +28,8 @@ class BackendController extends BaseController
             'token' => 'required|exists:records'
         ]);
 
-        $client = new GuzzleHttp\Client(['base_uri' => 'https://api.cloudflare.com']);
-
-        $key = json_decode($client->request('GET', '/client/v4/zones?name='.env('CLOUDFLARE_DOMAIN').'&status=active&page=1&per_page=1', [
-            'headers' => [
-                'X-Auth-Email' => env('CLOUDFLARE_EMAIL'),
-                'X-Auth-Key' => env('CLOUDFLARE_KEY')
-            ]
-        ])->getBody()->getContents())->result['0']->id;
-
-        $response = $client->request('DELETE', '/client/v4/zones/'.$key.'/dns_records/'.$request->input('token'), [
-            'headers' => [
-                'X-Auth-Email' => env('CLOUDFLARE_EMAIL'),
-                'X-Auth-Key' => env('CLOUDFLARE_KEY')
-            ]
-        ]);
-
-        DB::table('records')->where(['token' => $request->input('token')])->delete();
+        $flare = new FlareService;
+        $flare->delete($request->input('token'));
 
         return redirect()->route('index')->with('delete', 1);
     }
---
+++ app/Services/FlareService.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Services;
+
+use DB;
+use GuzzleHttp;
+
+class FlareService
+{
+	/**
+     * Get the zone identifier of a domain in cloudflare.
+     *
+     * @return string
+     */
+    public function zone()
+    {
+		$client = new GuzzleHttp\Client(['base_uri' => 'https://api.cloudflare.com']);
+
+        $zone = json_decode($client->request('GET', '/client/v4/zones?name='.env('CLOUDFLARE_DOMAIN').'&status=active&page=1&per_page=1', [
+            'headers' => [
+                'X-Auth-Email' => env('CLOUDFLARE_EMAIL'),
+                'X-Auth-Key' => env('CLOUDFLARE_KEY')
+            ]
+        ])->getBody()->getContents())->result['0']->id;
+
+		return $zone;
+    }
+
+    /**
+     * Create a Cloudflare record and store it in the database.
+     *
+     * @param string $fqdn
+     * @param string $ip
+     * @return string
+     */
+    public function create($fqdn, $ip)
+    {
+		$zone = $this->zone();
+
+		$client = new GuzzleHttp\Client(['base_uri' => 'https://api.cloudflare.com']);
+
+        $record = json_decode($client->request('POST', '/client/v4/zones/'.$zone.'/dns_records', [
+            'headers' => [
+                'X-Auth-Email' => env('CLOUDFLARE_EMAIL'),
+                'X-Auth-Key' => env('CLOUDFLARE_KEY')
+            ],
+            'json' => [
+                'type' => 'A',
+                'name' => $fqdn,
+                'content' => $ip
+            ]
+        ])->getBody()->getContents())->result->id;
+
+        DB::table('records')->insert(['token' => $record, 'fqdn' => $fqdn, 'ip' => $ip]);
+
+		return $record;
+    }
+
+	/**
+     * Delete a record and remove it from the database.
+     *
+     * @param string $fqdn
+     * @param string $ip
+     * @return void
+     */
+    public function delete($token)
+    {
+		$zone = $this->zone();
+
+		$client = new GuzzleHttp\Client(['base_uri' => 'https://api.cloudflare.com']);
+
+        $client->request('DELETE', '/client/v4/zones/'.$zone.'/dns_records/'.$token, [
+            'headers' => [
+                'X-Auth-Email' => env('CLOUDFLARE_EMAIL'),
+                'X-Auth-Key' => env('CLOUDFLARE_KEY')
+            ]
+        ]);
+
+        DB::table('records')->where(['token' => $token])->delete();
+    }
+}
