7eeb8fa6d163c12602f2e38e9e530f6b10c8f7f7
BlameDylan <dylan.seidt@gmail.com>
2016-01-17 13:54:27 -0600

    Allow Frontend or API to be disabled

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,12 +6,14 @@
 |--------------------------------------------------------------------------
 */
 
-$app->get('/', ['as' => 'index', 'uses' => 'FrontendController@getIndex']);
+if(env('APP_FRONTEND')) {
+	$app->get('/', ['as' => 'index', 'uses' => 'FrontendController@getIndex']);
 
-$app->post('/generate', ['as' => 'generate', 'middleware' => 'csrf', 'uses' => 'BackendController@postGenerate']);
-$app->post('/destroy', ['as' => 'destroy', 'middleware' => 'csrf', 'uses' => 'BackendController@postDestroy']);
+	$app->post('/generate', ['as' => 'generate', 'middleware' => 'csrf', 'uses' => 'BackendController@postGenerate']);
+	$app->post('/destroy', ['as' => 'destroy', 'middleware' => 'csrf', 'uses' => 'BackendController@postDestroy']);
+}
 
-$app->post('/api/generate', ['as' => 'generate', 'uses' => 'BackendController@apiPostGenerate']);
-$app->delete('/api/destroy', ['as' => 'destroy', 'uses' => 'BackendController@apiDeleteDestroy']);
-
-
+if(env('APP_API')) {
+	$app->post('/api/generate', ['as' => 'generate', 'uses' => 'BackendController@apiPostGenerate']);
+	$app->delete('/api/destroy', ['as' => 'destroy', 'uses' => 'BackendController@apiDeleteDestroy']);
+}
